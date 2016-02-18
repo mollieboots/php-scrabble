03d67c3f8d87e11a2eedbc42f24d168e72e1070a
Mollie Bootsma and Ryan Brown <mollie.bootsma@gmail.com>
2016-02-18 10:18:52 -0800

    Add results.html.twig page.

--- app/app.php
+++ app/app.php
@@ -4,10 +4,18 @@
 
     $app = new Silex\Application();
 
+    $app['debug'] = true;
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get('/', function() use ($app) {
         return $app['twig']->render('homepage.html.twig');
+    });
+
+    $app->get('/results', function() use ($app) {
+        $scrabble_word = new ScrabbleGenerator;
+        $result = $scrabble_word->scrabble_generator($_GET['word']);
+        return $app['twig']->render('results.html.twig', array('results' => $result));
     });
 
     return $app;
