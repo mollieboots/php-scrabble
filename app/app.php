f0c693a55ad50124758fa8aef7006ff2a22047b2
Mollie Bootsma and Ryan Brown <browneryan87@gmail.com>
2016-02-18 09:31:43 -0800

    Started silex

---
+++ app/app.php
@@ -0,0 +1,14 @@
+<?php
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/ScrabbleGenerator.php';
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('homepage.html.twig');
+    });
+
+    return $app;
+ ?>
